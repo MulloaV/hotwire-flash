76aee762d9bc31168196d865df15962a3b6c6544
MsulloaV <datsebashop@gmail.com>
2022-03-07 04:13:49 +0000

    SPA w/ realtime alerts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,19 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        flash.now[:notice] = "#{@post.id} added at #{Time.zone.now}"
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("new_post", partial: 'posts/form', locals: {post: Post.new}),
+            turbo_stream.prepend("posts", partial: "posts/post", locals: {post: @post }),
+            turbo_stream.update("flash", partial: "layouts/flash")
+          ]
+        end
       else
+        format.turbo_stream do
+          render turbo_stream:
+            turbo_stream.update("new_post", partial: 'posts/form', locals: {post: @post})
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
